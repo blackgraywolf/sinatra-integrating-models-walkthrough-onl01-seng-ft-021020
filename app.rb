2b7054dbbc7619cae8a7b84ce34a9297a5147339
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-26 18:11:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
-
+post '/' do
+  text_from_user = params[:user_text]
+ 
+  @analyzed_text = TextAnalyzer.new(text_from_user)
+ 
+  erb :results
+end
   post '/' do
   @analyzed_text = TextAnalyzer.new(params[:user_text])
  
